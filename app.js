13e44c4f2aaf6f5bdaa506165ac1fb993429987f
Chisquare7 <chijiokechibuike.f@gmail.com>
2024-08-14 23:18:49 +0100

    Implemented MongoDB store and CSS for dashboard, add-product, edit product pages

--- app.js
+++ app.js
@@ -1,198 +1,246 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const { connectDatabase } = require("./config/mongoose");
-const {adminAuthenticator} = require("./adminAuthenticator/adminAuth")
+const { adminAuthenticator } = require("./adminAuthenticator/adminAuth");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const adminRoute = require("./admin/adminRoute");
-const productRoute = require("./products/productRoute")
+const productRoute = require("./products/productRoute");
 const productModel = require("./models/productModel");
-const variationModel = require("./models/variationModel")
-
+const variationModel = require("./models/variationModel");
 
 const app = express();
 require("dotenv").config();
 
 connectDatabase();
 
-app.locals.appName = "Barb Hub"
+app.locals.appName = "Barb Hub";
 
 app.set("view engine", "ejs");
-app.set("views", "views")
+app.set("views", "views");
 
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/public", express.static("public"));
 app.use("/admin", adminRoute);
 app.use("/products", productRoute);
-app.use(session({
+
+const store = new MongoDBStore({
+  uri: process.env.DB_URL,
+  collection: "sessions"
+});
+
+store.on("error", function(error) {
+  console.error("Session store error:", error)
+});
+
+app.use(
+  session({
+    store: store,
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: {secure: false}
-}))
+    cookie: { secure: false },
+  })
+);
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy-Report-Only",
+    "default-src 'self'; script-src 'self' 'unsafe-inline' https://code.jquery.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://stackpath.bootstrapcdn.com https://cdnjs.cloudflare.com https://fonts.googleapis.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' https://i.ibb.co; frame-src 'self'"
+  );
+
+  next();
+});
 
 app.get("/", async (req, res) => {
-    try {
-        const cartItems = req.session.cartItems || {};
-        const filter = req.query.product_state || "Published";
+  try {
+    const cartItems = req.session.cartItems || {};
+    const filter = req.query.product_state || "Published";
 
-        const query = {
-            product_state: filter
-        };
+    const query = {
+      product_state: filter,
+    };
 
-        const productDetails = await productModel.find(query);
+    const productDetails = await productModel.find(query);
 
-        res.status(200).render("home", {
-          navs: ["Home", "Products", "Login"],
-          productDetails,
-          cartItems,
-        });
-    } catch (error) {
-        console.error("Error retrieving Products:", error);
-        res.status(500).send("Internal Server Error");
-    }
+    res.status(200).render("home", {
+      navs: ["Home", "Products", "Login"],
+      productDetails,
+      cartItems,
+    });
+  } catch (error) {
+    console.error("Error retrieving Products:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/home", (req, res) => {
-    res.redirect("/")
-})
+  res.redirect("/");
+});
 
 app.get("/register", (req, res) => {
-    return res.render("register", {
-        navs: ["Home", "Login"]
-    })
+  return res.render("register", {
+    navs: ["Home", "Login"],
+  });
 });
 
 app.get("/login", (req, res) => {
-    return res.render("login", {
-        navs: ["Home", "Register"]
-    });
+  return res.render("login", {
+    navs: ["Home", "Register"],
+  });
 });
 
 app.get("/dashboard", adminAuthenticator, async (req, res) => {
+  const productDetails = await productModel.find({
+    admin_id: res.locals.admin._id,
+  });
 
-    const productDetails = await productModel.find({admin_id: res.locals.admin._id});
+  console.log("Product details:", productDetails);
 
-    console.log("Product details:", productDetails);
-
-    res.status(200).render("dashboard", {
-        navs: ["Dashboard", "Add Product", "Products", "Logout" ],
-        admin: res.locals.admin,
-        productDetails
-    });
+  res.status(200).render("dashboard", {
+    navs: ["Dashboard", "Add Product", "Products", "Logout"],
+    admin: res.locals.admin,
+    productDetails,
+  });
 });
-
 
 app.get("/addProduct", adminAuthenticator, async (req, res) => {
-    res.status(200).render("addProduct", {
-        navs: ["Dashboard", "Products", "Logout"],
-        admin: res.locals.admin
-    })
-})
-
-app.get("/edit/:id", adminAuthenticator, async (req, res) => {
-    const id = req.params.id;
-
-    try {
-        const product = await productModel.findById(id);
-
-        if (!product) {
-            res.redirect("/404ErrorPage");
-            return;
-        }
-
-        res.render("editProduct", {
-            navs: ["Dashboard", "Products", "Logout"],
-            admin: res.locals.admin,
-            productToEdit: product
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
+  res.status(200).render("addProduct", {
+    navs: ["Dashboard", "Products", "Logout"],
+    admin: res.locals.admin,
+  });
 });
 
+app.get("/edit/:id", adminAuthenticator, async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const product = await productModel.findById(id);
+
+    if (!product) {
+      res.redirect("/404ErrorPage");
+      return;
+    }
+
+    res.render("editProduct", {
+      navs: ["Dashboard", "Products", "Logout"],
+      admin: res.locals.admin,
+      productToEdit: product,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 
 app.get("/products", async (req, res) => {
-    try {
-        const filter = req.query.product_state || "Published";
+  try {
+    const cartItems = req.session.cartItems || {};
+    const filter = req.query.product_state || "Published";
 
-        const query = {
-            product_state: filter
-        };
+    const query = {
+      product_state: filter,
+    };
 
-        const productDetails = await productModel.find(query);
+    const productDetails = await productModel.find(query);
 
-        res.status(200).render("products", {
-            navs: ["Home", "Products"],
-            productDetails,
-        })
-    } catch (error) {
-        console.error("Error retrieving Products:", error);
-        res.status(500).send("Internal Server Error")
-    }
+    res.status(200).render("products", {
+      navs: ["Home", "Products"],
+      productDetails,
+      cartItems,
+    });
+  } catch (error) {
+    console.error("Error retrieving Products:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/products/:id", async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    try {
-        const product = await productModel.findById(id).populate('variations')
+  try {
+    const cartItems = req.session.cartItems || {};
+    const product = await productModel.findById(id).populate("variations");
 
-        if (!product) {
-            res.redirect("/404ErrorPage");
-            return
-        };
-
-        res.render("oneProduct", {
-            navs: ["Home", "Products"],
-            admin: res.locals.admin,
-            oneProduct: product
-        })
-    } catch (error) {
-        console.error("Error retrieving one product:", error);
-        res.status(500).send(error);
+    if (!product) {
+      res.redirect("/404ErrorPage");
+      return;
     }
+
+    res.render("oneProduct", {
+      navs: ["Home", "Products"],
+      admin: res.locals.admin,
+      oneProduct: product,
+      cartItems,
+    });
+  } catch (error) {
+    console.error("Error retrieving one product:", error);
+    res.status(500).send(error);
+  }
 });
 
 app.get("/orders", (req, res) => {
-    const cartItems = req.session.cartItems || {};
+  const cartItems = req.session.cartItems || {};
 
-    if (Object.keys(cartItems).length === 0) {
-        res.status(200).render("emptyCart");
-    } else {
-        res.status(200).render("orders", {cartItems})
-    }
-})
+  if (Object.keys(cartItems).length === 0) {
+    res.status(200).render("emptyCart");
+  } else {
+    res.status(200).render("orders", { cartItems });
+  }
+});
 
 app.post("/add-to-cart", (req, res) => {
-    const {productId, price, productName, productImage, productSize = "No size", productColor = "No color"} = req.body;
-    if (!productId || !price || !productName || !productImage) {
-        console.error("Missing required fields:", req.body);
-      return res
-        .status(400)
-        .json({ success: false, message: "Missing required fields" });
-    }
+  const {
+    productId,
+    price,
+    productName,
+    productImage,
+    productSize = "No size",
+    productColor = "No color",
+  } = req.body;
+  if (!productId || !price || !productName || !productImage) {
+    console.error("Missing required fields:", req.body);
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing required fields" });
+  }
 
-    const cartItems = req.session.cartItems || {};
-    cartItems[productId] = {price, productName, productImage, productSize, productColor};
-    req.session.cartItems = cartItems;
+  const cartItems = req.session.cartItems || {};
+  if (!cartItems[productId]) {
+    cartItems[productId] = {
+      price: parseFloat(price),
+      productName,
+      productImage,
+      productSize,
+      productColor,
+      quantity: 1,
+    };
+  } else {
+    cartItems[productId].quantity = (cartItems[productId].quantity || 0) + 1;
+  }
+  req.session.cartItems = cartItems;
 
-    res.json({success: true})
+  res.json({ success: true });
 });
 
 app.post("/remove-from-cart", (req, res) => {
-    const {productId} = req.body;
-    const cartItems = req.session.cartItems || {};
+  const { productId } = req.body;
+  const cartItems = req.session.cartItems || {};
 
-    delete cartItems[productId];
-    req.session.cartItems = cartItems;
+  if (cartItems[productId]) {
+    cartItems[productId].quantity = (cartItems[productId].quantity || 0) - 1;
+    
+    if (cartItems[productId].quantity <= 0) {
+      delete cartItems[productId];
+    }
+  }
+  req.session.cartItems = cartItems;
 
-    res.json({ success: true });
-})
+  res.json({ success: true });
+});
 
 app.get("/logout", (req, res) => {
   res.clearCookie("jwt");
