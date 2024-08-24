2a7f1291333574e4cb49cf605d04a9c7743a27fe
Chisquare7 <chijiokechibuike.f@gmail.com>
2024-08-24 11:44:17 +0100

    Readjust edit product endpoints in the controller and ejs

--- products/productControllers.js
+++ products/productControllers.js
@@ -1,5 +1,6 @@
 const productModel = require("../models/productModel")
 const variationModel = require("../models/variationModel");
+const mongoose = require("mongoose");
 
 
 const addProduct = async ({name, description, price, product_image, stock_level, product_state, variations, admin_id}) => {
@@ -34,29 +35,49 @@ const addProduct = async ({name, description, price, product_image, stock_level,
 
 
 const editProduct = async (req, res) => {
-    const productId = req.params.id;
-    const update = req.body;
 
-    productModel.findByIdAndUpdate(productId, update, {new: true})
-        .then((updatedProduct) => {
-            if (!updatedProduct) {
-                return res.redirect("/dashboard")
-            }
+    try {
+        const productId = req.params.id;
+        const update = req.body;
 
-            if (update.variations) {
-                return variationModel.deleteMany({product_id: productId})
-                    .then(() => {
-                        const variations = update.variations.map(variation => ({...variation, productId}))
-                        return variationModel.insertMany(variations)
-                    })
-            }
-        }).then(() => {
-            res.redirect("/dashboard")
-        })
-        .catch((error) => {
-            console.log(error)
-            res.status(500).send(error)
-        })
+        const variations = [];
+        for (let i = 0; update[`variations[${i}][size]`]; i++) {
+          variations.push({
+            size: update[`variations[${i}][size]`],
+            color: update[`variations[${i}][color]`],
+            additionalDetails: update[`variations[${i}][additionalDetails]`],
+            product_id: mongoose.Types.ObjectId.createFromHexString(productId),
+          });
+        }
+
+        const updatedProduct = await productModel.findByIdAndUpdate(
+            productId,
+            update,
+            {new: true}
+        );
+
+        if (!updatedProduct) {
+            return res.redirect("/dashboard")
+        }
+
+        console.log("Product successfully updated:", updatedProduct);
+
+        if (variations.length > 0) {
+
+            await variationModel.deleteMany({product_id: mongoose.Types.ObjectId.createFromHexString(productId)});
+
+            const newVariations = await variationModel.insertMany(variations);
+
+            updatedProduct.variations = newVariations.map(variation => variation._id);
+            await updatedProduct.save();
+
+        }
+
+        res.redirect("/dashboard")
+    } catch (error) {
+        console.error("Error encountered during update process", error);
+        res.status(500).send(error)
+    }
 }
 
 
